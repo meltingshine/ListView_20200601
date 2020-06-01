c4fc4101d7ff38cf82dd62c25c59fe5645d9fd65
meltingshine <meltingshine@gmail.com>
2020-06-01 21:48:40 +0900

    목록 변수(ArrayList)만들고 실 데이터 추가

--- app/src/main/java/com/example/listview_20200601/MainActivity.kt
+++ app/src/main/java/com/example/listview_20200601/MainActivity.kt
@@ -2,8 +2,12 @@ package com.example.listview_20200601
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.listview_20200601.datas.Student
 
 class MainActivity : BaseActivity() {
+    val students = ArrayList<Student>()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -16,6 +20,12 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setValues() {
+
+        students.add(Student("조경진","1988",true))
+        students.add(Student("김미영","2010",false))
+        students.add(Student("김성우","1982",true))
+        students.add(Student("이용희","1992",true))
+        students.add(Student("이지수","1984",false))
 
     }
 }
